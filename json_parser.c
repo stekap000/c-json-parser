29f744428b54c6ae68707fda9605e4cc35879aa1
stekap000 <stekap000@gmail.com>
2024-10-01 22:29:01 +0200

    temp save

--- json_parser.c
+++ json_parser.c
@@ -1,5 +1,17 @@
 typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
 typedef unsigned long long u64;
+
+typedef char s8;
+typedef short s16;
+typedef int s32;
+typedef long long s64;
+
+typedef float f32;
+typedef double f64;
+
+typedef unsigned int b32;
 
 typedef struct buffer {
 	u64 size;
@@ -36,6 +48,29 @@ typedef struct {
 	u64 at;
 } JSON_Parser;
 
-JSON_Token JSON_next_token() {
+typedef struct {
+	JSON_Token token;
+	struct JSON_Node* first_child;
+	struct JSON_Node* next_sibling;
+} JSON_Node;
+
+b32 valid_parser_position(JSON_Parser* parser) {
+	return (parser->at < parser->source.size);
+}
+
+void skip_white_space(JSON_Parser* parser) {
+	u8* data = parser->source.data;
+	u64 at = parser->at;
+
+	while(valid_parser_position(parser) && data[at] == ' ' && data[at] == '\t' && data[at] == '\n' && data[at] == '\r') {
+		parser->at++;
+	}
+}
+
+JSON_Token JSON_next_token(JSON_Parser* parser) {
+	skip_white_space(parser);
+
+	
+	
 	return (JSON_Token){};
 }
